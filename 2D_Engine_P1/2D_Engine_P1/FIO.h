69a0efc691d6434b14d94ab8e02de6381358c7c0
kylecodes <kylemuldoon15@gmail.com>
2019-10-16 22:37:14 -0700

    f

--- 2D_Engine_P1/2D_Engine_P1/FIO.h
+++ 2D_Engine_P1/2D_Engine_P1/FIO.h
@@ -1,19 +1,44 @@
+// TODO: COPY FROM OLD C++ PROJECT
+
 #ifndef FIO_H
 #define FIO_H
 #include <iostream>
+#include <fstream>
 #include <string>
 
 using namespace std;
 
 class FIO
 {
+public:
 	// Data
+	string inputfileName;
+	string outputFileName;
 
 	// Constructor
+	FIO();
+	FIO(string);
+	~FIO();
 
-	// Destructor
+	// SETTERS 
+	void changeFilePath(string);
+	void changeOutputName(string);
+
+	// FILE OPERATIONS
+
 };
 
+FIO::FIO()
+{
+	inputfileName = "testfile";
+}
+
+FIO::FIO(string newName)
+{
+	inputfileName = newName;
+}
+
+FIO::~FIO() {}
 
 
 #endif
